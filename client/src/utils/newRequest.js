808f792e547c1524bae8c796f77177082c8df99c
niranjanreddy05 <niranjan2r22@gmail.com>
2025-06-24 22:14:37 +0530

    Updated API base URL in axios config

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "https://kisan-mart-backend.onrender.com/",
+  baseURL: "https://kisanmart-dqd7.onrender.com",
   //baseURL: "https://api-h2x3.onrender.com/api/",
 //  baseURL: "https://agrosync-prototype.vercel.app/api/",
   withCredentials: true,
