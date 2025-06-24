1dfd3c3e27f48d9a0cd41127cb95eee044c46ff1
niranjanreddy05 <niranjan2r22@gmail.com>
2025-06-24 22:29:45 +0530

    resolved cors error

--- api/server.js
+++ api/server.js
@@ -21,7 +21,7 @@ const connect = async () => {
       console.log(error);
     }
   };
-  app.use(cors({ origin: ['http://localhost:5173', 'https://agrosync-prototype.vercel.app'], credentials: true }));
+  app.use(cors({ origin: ["https://kisan-mart-tau.vercel.app", 'https://agrosync-prototype.vercel.app'], credentials: true }));
   app.use(express.json());
   app.use(cookieParser());
 
